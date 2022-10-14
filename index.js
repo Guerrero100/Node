0957688e64767c882ae3196bcd572d9a945217f1
unknown <SENA@ITAAPRREAFSD424.FormacionSena.RED>
2022-10-14 17:34:50 -0500

    fase2 Node

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const os = require('os')
 const express = require('express');
 const servidor = express();
 const port = 3001;
 
 servidor.get('/', (req, res)=>{
-    res.send(`Proyecto ${port}`);
+    res.send(`Proyecto ${port}
+    <br>
+    ${os.tmpdir()}
+    `);
     
 
 })
